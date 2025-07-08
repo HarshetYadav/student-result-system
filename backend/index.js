b1aad4cf38c4d6823eeaac0fa9d47bddd58903bb
HarshetYadav <harshityadav28541@gmail.com>
2025-07-08 14:34:17 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
-const mysql = require('mysql');
+const mysql = require('mysql2');
 require('dotenv').config(); // ✅ Keep this line
 
 // Serve static files from the frontend and images folders
